ef66e7a77d717a8197e0cf988fdbffe708029daa
arpithasureshappa <arpitha.sureshappa@tarento.com>
2025-07-14 13:55:55 +0530

    updateAttributes value check

--- src/main/java/com/igot/cb/cassandra/utils/CassandraOperationImpl.java
+++ src/main/java/com/igot/cb/cassandra/utils/CassandraOperationImpl.java
@@ -146,6 +146,7 @@ public class CassandraOperationImpl implements CassandraOperation {
 
 	public Map<String, Object> updateRecord(String keyspaceName, String tableName, Map<String, Object> updateAttributes,
 											Map<String, Object> compositeKey) {
+		logger.info("Updating record in table: {} with attributes: {}", tableName, updateAttributes);
 		Map<String, Object> response = new HashMap<>();
 		CqlSession session = null;
 		try {
