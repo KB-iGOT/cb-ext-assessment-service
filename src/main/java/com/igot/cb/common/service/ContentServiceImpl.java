8d42bc4e7edb0f2608183801ce489e6b6746df59
arpithasureshappa <arpitha.sureshappa@tarento.com>
2025-08-29 12:15:28 +0530

    KB-10631 Prograss update for preenroll assessment

--- src/main/java/com/igot/cb/common/service/ContentServiceImpl.java
+++ src/main/java/com/igot/cb/common/service/ContentServiceImpl.java
@@ -184,6 +184,56 @@ public class ContentServiceImpl implements ContentService{
         return readContent(contentId, Collections.emptyList());
     }
 
+    @Override
+    public String updatePreEnrolledAssessment(String userAuthToken, Map<String, Object> reqBody, String userId, SBApiResponse outgoingResponse) {
+        String response = "";
+        try {
+            Map<String, String> headers = new HashMap<>();
+            headers.put(Constants.CONTENT_TYPE, Constants.APPLICATION_JSON);
+            headers.put(Constants.X_AUTH_TOKEN, userAuthToken);
+
+
+            Map<String, Object> req = new HashMap<>();
+            Map<String, Object> request = new HashMap<>();
+            List<Map<String, Object>> contents = new ArrayList<>();
+
+            Map<String, Object> reqObj = new HashMap<>();
+            reqObj.put(Constants.CONTENT_ID_KEY, reqBody.get(Constants.IDENTIFIER));
+
+            reqObj.put(Constants.STATUS, 2);
+            reqObj.put("lastAccessTime", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSSZ").format(new Date()));
+            reqObj.put(Constants.COMPLETION_PERCENTAGE, 100);
+            reqObj.put(Constants.PROGRESS_DETAILS, new HashMap<>());
+
+            contents.add(reqObj);
+
+            req.put("contents", contents);
+            request.put(Constants.REQUEST,req);
+
+            Map<String, Object> apiResponse = outboundRequestHandlerService.fetchResultUsingPatch(
+                    serverConfig.getExtCourseServiceHost() + serverConfig.getContentStateUpdate(),
+                    request, headers);
+
+            if ("OK".equals(apiResponse.get("responseCode"))) {
+                response = Constants.SUCCESS;
+                logger.info(String.format("Successfully updated progress for user : %s, for assessment : %s", userId,
+                        reqBody.get(Constants.IDENTIFIER)));
+            } else {
+                logger.info(String.format("Failed to update progress for user : %s, for assessment : %s, of course :%s", userId,
+                        reqBody.get(Constants.IDENTIFIER),reqBody.get(Constants.COURSE_ID)));
+                outgoingResponse.setResult(null);
+                updateErrorDetails(outgoingResponse, Constants.FAILED_TO_UPDATE_PROGRESS, HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+
+        } catch (Exception e) {
+            logger.error(String.format("Failed to update progress for user: %s, for assessment: %s, of course: %s. Exception: %s",
+                    userId, reqBody.get(Constants.IDENTIFIER),reqBody.get(Constants.COURSE_ID), e.getMessage()), e);
+            outgoingResponse.setResult(null);
+            updateErrorDetails(outgoingResponse, Constants.FAILED_TO_UPDATE_PROGRESS, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return response;
+    }
+
     public Map<String, Object> readContent(String contentId, List<String> fields) {
         StringBuilder url = new StringBuilder();
         url.append(serverConfig.getContentHost()).append(serverConfig.getContentReadEndPoint()).append("/" + contentId)
